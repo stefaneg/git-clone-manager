b9453167146db30618e1817ad5e66fee0800dce3
gudlaugur.egilsson <gudlaugur.egilsson@controlant.com>
2025-03-15 14:11:41 +0000

    Add package docs

---
+++ internal/ext/doc.go
@@ -0,0 +1,5 @@
+// Package ext provides various language extensions that enhance the Go standard library.
+//
+// This package includes functions for mathematical operations, path manipulations,
+// default value handling, and synchronization utilities with debugging capabilities.
+package ext
---
+++ internal/fs/doc.go
@@ -0,0 +1,5 @@
+package fs
+
+// Package fs abstracts file system operations used in git-clone-manager, and provides a way to unit test logic
+// fs operations without actually interacting with the filesystem.
+// Can also be thought of as the port into file system operations.
